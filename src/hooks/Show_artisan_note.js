0c46644ccbff972e381e6612e105aa53dc96994c
david <tyson.b@hotmail.fr>
2024-03-31 12:14:33 +0200

    Updates

--- src/hooks/Show_artisan_note.js
+++ src/hooks/Show_artisan_note.js
@@ -1,11 +1,7 @@
-import { useState, useEffect } from 'react';
-
-// Import icon
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
 function ShowArtisanNote() {
-const [artisans, setArtisans] = useState([]);
+  const [artisans, setArtisans] = useState([]);
 
   useEffect(() => {
     fetch("/datas.json")
@@ -23,19 +19,7 @@ const [artisans, setArtisans] = useState([]);
       );
   }, []);
 
-  // Fonction pour afficher les étoiles en fonction de la note
-  const renderStars = (note) => {
-    const starCount = Math.round(note); // Arrondir la note à l'entier le plus proche
-    const stars = [];
-    for (let i = 0; i < starCount; i++) {
-      stars.push(
-        <FontAwesomeIcon icon={faStar} key={i} className="star-icon" />
-      );
-    }
-    return stars;
-  };
-
-  return { artisans, renderStars };
+  return { artisans };
 }
 
 export default ShowArtisanNote;
